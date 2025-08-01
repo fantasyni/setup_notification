7bf13dc74c5398f77af0cf217c4601b84f3f2be5
justin <justin@163.com>
2025-08-01 17:26:13 +0800

    add failure build log

--- src/send.ts
+++ src/send.ts
@@ -1,5 +1,6 @@
 import { NotificationClient } from '@caasxyz/notification-sdk';
 import * as core from '@actions/core';
+import fs from 'fs';
 
 class NotificationService {
     private subject: string;
@@ -46,11 +47,20 @@ class NotificationService {
 async function main() {
     const user_id = core.getInput('user-id', { required: true });
     const api_key = core.getInput('api-key', { required: true });
-    const content = core.getInput('content', { required: true });
+    let content = core.getInput('content');
+    const failure = core.getInput('failure');
 
     let github_repo = `https://github.com/${process.env.GITHUB_REPOSITORY}`;
     let service = new NotificationService(`${github_repo} 打包`, api_key, user_id);
 
+    if (failure) {
+        let build_log_path = `${process.cwd()}/build.log`;
+        if (fs.existsSync(build_log_path)) {
+            let build_log = fs.readFileSync(build_log_path).toString();
+            content = `失败 \n ${build_log}`;
+        }
+    }
+    
     await service.send_content(content);
 }
 
